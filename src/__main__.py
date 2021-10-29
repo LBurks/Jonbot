a243af1ec4ee37b83cbe33f3c973ef82331965d6
Loren <lburks14@hotmail.com>
2021-10-28 21:03:43 -0500

    Added roster.py and some more environment variables

--- src/__main__.py
+++ src/__main__.py
@@ -6,6 +6,8 @@ from discord.ext import commands
 
 from dotenv import load_dotenv
 
+from roster import roster
+
 
 def startup():
     txt = [l[:-1] for l in open('./random.txt')]
@@ -13,14 +15,16 @@ def startup():
 
 txt = startup()
 
-bot = commands.Bot(command_prefix='.')
-
-@bot.command(help='Give a perfectly valid explanation for why you just lost.')
-async def john(ctx):
-    await ctx.send(choice(txt))
-
 load_dotenv()
 token = os.getenv('TOKEN')
+bot = commands.Bot(command_prefix=os.getenv('PREFIX'))
+
+@bot.command(help='Give a perfectly valid explanation for why you just lost.')
+async def john(ctx, test: int = 0):
+    if os.getenv('ENV') == 'dev': response = txt[test]
+    else: response = choice(txt)
+    await ctx.send(response)
+
 
 bot.run(token)
 
